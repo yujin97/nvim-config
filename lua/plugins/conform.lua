fd1e0f3c0b5aedfb38e3ffd8a7bff80844e77f25
eugene <eugene97@connect.hku.hk>
2025-10-22 23:39:42 +0900

    feat: add user command for toggling format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,6 +15,9 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       local disable_filetypes = { c = true, cpp = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
@@ -35,4 +38,25 @@ return {
       json = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
