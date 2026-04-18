bc2fdf60b79f94f16f3a279b67ff9a6656097ef6
eugene <eugene97@connect.hku.hk>
2026-04-18 16:19:32 +0900

    feat: add format target

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -36,6 +36,7 @@ return {
       typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       css = { 'prettierd', 'prettier', stop_after_first = true },
       json = { 'prettierd', 'prettier', stop_after_first = true },
+      jsonc = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
   config = function(_, opts)
